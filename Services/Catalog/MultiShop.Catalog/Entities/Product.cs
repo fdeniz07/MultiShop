77e91e23abae65f5a1ba43e28db98969c3417f76
Fatih Deniz <deniz.fatih@hotmail.de>
2024-03-31 05:11:09 +0200

    Entities

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+
+
+        [BsonIgnore] //Serilestirme isleminde (JSON tipine aktarmada) ilgili Collection(Tabloya)'a ait özelligin atlanmasi saglar.
+        public Category Category { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Info { get; set; }
+
+        [BsonIgnore]
+        public Product Product { get; set; }
+    }
+}

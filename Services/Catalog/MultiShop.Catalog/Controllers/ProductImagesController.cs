004b9acccb447705470bfc353d52e065f842353f
Fatih Deniz <deniz.fatih@hotmail.de>
2024-04-01 00:22:23 +0200

    ProductImagesController

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _productImageService;
+
+        public ProductImagesController(IProductImageService productImageService)
+        {
+            _productImageService = productImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _productImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = await _productImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _productImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün resmi basariyla eklendi");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _productImageService.DeleteProductImageAsync(id);
+            return Ok("Ürün resmi basariyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _productImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Ürün resmi basariyla güncellendi.");
+        }
+    }
+}

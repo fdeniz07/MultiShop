599bfb1a9c7a0342f66383077bdecb8802d3708c
Fatih Deniz <deniz.fatih@hotmail.de>
2024-04-01 00:11:54 +0200

    Category Controller

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Services.CategoryServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryService.GetAllCategoryAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(string id)
+        {
+            var values = await _categoryService.GetByIdCategoryAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori basariyla eklendi");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return Ok("Kategori basariyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return Ok("Kategori basariyla güncellendi.");
+        }
+    }
+}
